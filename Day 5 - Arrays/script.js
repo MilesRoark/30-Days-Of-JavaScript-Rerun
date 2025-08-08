95f89fa652ca5e163ad20919548167f2731a1c38
milesroark <babatunjia@gmail.com>
2025-08-08 05:11:57 -0400

    started day 5

---
+++ Day 5 - Arrays/script.js
@@ -0,0 +1,34 @@
+//-----------------------------------Level 1-------------------------------
+// Declare an empty array.
+
+// Declare an array with more than 5 elements:
+//              Find the length of the array.
+//              Get the first, middle and last items of the array.
+
+// Declare an array of more than 5 items:
+//              Name the array mixedDataTypes.
+//              Populate the array with different data types.
+//              Find the length of the array.
+
+// Declare an array called itCompanies:
+//              assign the values:
+//                  Google, Microsoft, Apple, OpenAi, Amazon, Meta, Anthropic
+//              Print the contents of the array to the console.
+//              How many companies are in the array?
+//              Print the first, middle and last company.
+//              Print each company.
+//              Change each companies name to uppercase and print the result.
+//              print "{company names} are current players in tech."
+//              Check if a company exists in the array.
+//                      If it exists return the companies name
+//                      If not return "company not found."
+//              Filter out companies with multiple o's in their names without using the filter method.
+//              Use the sort() method on the array.
+//              Use the reverse() method on the array.
+//              slice out the first 3 companies.
+//              slice out the last three companies.
+//              slice out the middle company or companies.
+//              Remove the first company.
+//              Remove the middle company or companies.
+//              Remove the last company.
+//              Clear the array.
