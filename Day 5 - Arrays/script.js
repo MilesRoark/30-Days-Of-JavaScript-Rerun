f87c024a96bacc6ff80b517c89651fac23f2b080
milesroark <babatunjia@gmail.com>
2025-08-10 02:30:05 -0400

    Finished building exercise

--- Day 5 - Arrays/script.js
+++ Day 5 - Arrays/script.js
@@ -32,3 +32,48 @@
 //              Remove the middle company or companies.
 //              Remove the last company.
 //              Clear the array.
+
+// ---------------------------------Level 2---------------------------------------
+
+// Create 2 new files:
+//              A countries.js file to store the countries array.
+//              A web_techs.js file to store the webTechs array.
+//              Access both files in the main script file.
+
+// "I love teaching and empowering people. I teach HTML, CSS, JS, React, Python."
+//              Create an array of all the words in the sentence without the punctuation.
+//              How many words are in the sentence?
+
+// Given this shopping list:
+//              ["MilK", "Coffee", "Tea", "Honey"]
+//                      add "Meat" as the first item on the list.
+//                      add "Sugar" as the last item on the list
+//                      remove "Honey" from the list.
+//                      change "Tea" to "Earl Grey"
+
+// Check if Ethiopia exists in the countries array, if not add it.
+
+// Check if Sass exists in the webTechs array. If not add it and print the array.
+
+// Concatenate these two arrays:
+//              const frontEnd = ["HTML", "CSS", "JS", "React", "Redux"]
+//              const backEnd = ["Node", "Express", "MongoDB"]
+//---------------------------------------Level 3----------------------------------
+
+// Complete the following exercises using this array:
+//              const ages = [19, 22,19, 24, 20, 25, 26, 24, 25, 24 ]
+//                      Sort the array
+//                      Find the min and max age
+//                      Find the median age(s)
+//                          formula: median = middle age(s) / 2
+//                      Find the average age
+//                          formula: average = sum of ages / number of ages
+//                      Find the range
+//                          formula: range = max - min
+//                      Compare the value of (min - average) and (max - average), use Math.abs() method.
+
+// Slice the first ten countries from the countries array.
+
+// Find the middle country(ies).
+
+// Split the array in to two different arrays.
