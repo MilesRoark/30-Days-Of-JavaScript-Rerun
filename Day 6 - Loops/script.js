f28fca7e20c0dad0ca6b24ceae06e11453a23f8c
milesroark <babatunjia@gmail.com>
2025-08-26 20:38:47 -0400

    Started Day 6

---
+++ Day 6 - Loops/script.js
@@ -0,0 +1,145 @@
+//======================== Level 1 ==============================
+// Iterate from 0 to 10 using a for loop, a whileloop and a do whileloop.
+
+// Iterate from 10 to 0 using a for loop, a whileloop and a do whileloop.
+
+// Iterate from 0 to n using a for loop.
+
+// Create this pattern in the console using a for loop:
+// @
+// @@
+// @@@
+// @@@@
+// @@@@@
+// @@@@@@
+// @@@@@@@
+
+// log() the following in the console using a for loop:
+// 0 * 0 = 0
+// 1 * 1 = 1
+// 2 * 2 = 4
+// 3 * 3 = 9
+// 4 * 4 = 16
+// 5 * 5 = 25
+// 6 * 6 = 36
+// 7 * 7 = 49
+// 8 * 8 = 64
+// 9 * 9 = 81
+// 10 * 10 = 100
+
+// log() the following pattern using a loop:
+// i  i^2  i^3
+// 0   0    0
+// 1   1    1
+// 2   4    8
+// 3   9    27
+// 4   16   64
+// 5   25   125
+// 6   36   216
+// 7   49   343
+// 8   64   512
+// 9   81   729
+// 10  100  1000
+
+// Iterate from 0 to 100 with a for Loop and log() only the even numbers.
+
+// Iterate from 0 to 100 with a for Loop and log() on the odd numbers.
+
+// Iterate from 0 to 100 with a for Loop and log() only the prime numbers.
+
+// Iterate from 0 to 100 with a for Loop and add all the numbers. (ans. 5050)
+
+// Iterate from 0 to 100 with a for Loop and log() the sum of the even and the sum of all the odd numbers.
+
+// Create an array from the results.
+
+// Create a script that generates 5 random numbers.
+
+// Create a script which generates 5 random numbers, the numbers should be unique.
+
+// Create a small script which generates a random id with six characters.
+//              Example: 5j2khz
+
+//======================== Level 2 ==============================
+// Create a script which generates an id of however many characters.
+//              Ex1: fe3jo1gl124g
+//              Ex2: xkqci4utda1lmbelpkm03rba
+
+// Create a script that generates random hexadecimal number
+//              Example: #ee33df
+
+// Create a script that generates random rgb color numbers.
+//              Example: rgb(134, 163, 242)
+
+// Create an array of the lengths of the country names in the countries array.
+//              Example:[7, 7, 6, 7, 8, 7, 7, 7, 7, 5, 5]
+
+// Create the following array of arrays using the countries array
+//              [
+//                ['Albania', 'ALB', 7],
+//                ['Bolivia', 'BOL', 7],
+//                ['Canada', 'CAN', 6],
+//                ['Denmark', 'DEN', 7],
+//                ['Ethiopia', 'ETH', 8],
+//                ['Finland', 'FIN', 7],
+//                ['Germany', 'GER', 7],
+//                ['Hungary', 'HUN', 7],
+//                ['Ireland', 'IRE', 7],
+//                ['Iceland', 'ICE', 7],
+//                ['Japan', 'JAP', 5],
+//                ['Kenya', 'KEN', 5]
+//              ]
+
+// Using the countries array, log("") all the countries with the word "land" in their names to the console. If there are none then log("No country contains the word land")
+//              Example:['Finland','Ireland', 'Iceland']
+
+// Using the countries array, log("") all the countries with the string "ia" in their names to the console. If there are none then log("No country contains the word land")
+//              Example:['Albania', 'Bolivia','Ethiopia']
+
+// Find the country with the longest name.
+//                  Answer:Ethiopia
+
+// Find the country(s) with only 5 characters in their name.
+//                  Answer:['Japan', 'Kenya']
+
+// Find the longest word in the webTechs array.
+//              [
+//                ["HTML", 4],
+//                ["CSS", 3],
+//                ["JavaScript", 10],
+//                ["React", 5],
+//                ["Redux", 5],
+//                ["Node", 4],
+//                ["MongoDB", 7]
+//              ]
+// Using MongoDB, Express, React and Node together is called a MERN stack . Create an array of the acronym MERN.
+
+// Iterate through the webTechs array using a for() or a for Of() loop and log() the items.
+
+// Reverse the order of the provided array without using the reverse() method
+//          ['banana', 'orange', 'mango', 'lemon']
+
+// log() all the contents of the webTechs array without putting them in an array.
+//          HTML
+//          CSS
+//          JS
+//          REACT
+//          NODE
+//          EXPRESS
+//          MONGODB
+//======================== Level 3 ==============================
+// Using the countries2 array:
+//          Copy the array without mutating it.
+//          Sort the copy and store it in an array called sortedCountries.
+
+// sort the webTechs array and mernStack array.
+
+// put all the countries from the countries2 array with the word "land" in their name(s) in a new array.
+
+// Find the country(s) with the longest name in the countries2 array.
+
+// Put all the countries with just four letters in their name(s) in their own array.
+
+// Put all the countries with two or more words in their name(s) in their own array.
+
+// Create a new array with all the countries in the countries2 array in reverse order and capitalize the country names.
