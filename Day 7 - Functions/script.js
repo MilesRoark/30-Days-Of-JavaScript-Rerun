bb4fd3ffd6eda3e8e21bb605e28a13a9a89d4db1
milesroark <babatunjia@gmail.com>
2025-09-27 02:52:58 -0400

    finished first build

---
+++ Day 7 - Functions/script.js
@@ -0,0 +1,166 @@
+//*************************LEVEL 1******************************
+//#########################QUESTION 1###########################
+// Declare a function fullName that prints out your full name.
+
+//#########################QUESTION 2###########################
+// Add a firstName and a lastName parameter to the previous function.
+
+//#########################QUESTION 3###########################
+// Declare a function called addNumbers that takes two parameters and returns the sum.
+
+//#########################QUESTION 4###########################
+// area = length * width. Write an areaOfRectangle function.
+
+//#########################QUESTION 5###########################
+// perimeter = 2 * [length + width]. Write a perimeterOfRectangle function.
+
+//#########################QUESTION 6###########################
+// volume = length * width * height. write a volumeOfRectPrism function.
+
+//#########################QUESTION 7###########################
+// area[circle] = pi * r * r. write an areaOfCircle function.
+
+//#########################QUESTION 8###########################
+// circumference = 2 * pi * r. write a circumOfCircle function.
+
+//#########################QUESTION 9###########################
+// density = mass / volume. write a function that calculates density.
+
+//#########################QUESTION 10###########################
+// speed = distance / time. write a function that calculates speed.
+
+//#########################QUESTION 11###########################
+// weight = mass * gravity. write a function that calculates weight.
+
+//#########################QUESTION 12###########################
+// farenheit = [oC * 9/5] + 32. write a celciusToFarenheit converter function.
+
+//#########################QUESTION 13###########################
+// BMI = (weight in pounds / [height in inches]²) x 703. Write a weightGroup function that uses your BMI to determine your weight group.
+// 18.5 or lower BMI = Underweight
+// 18.5 to 24.9 BMI = Normal
+// 25 to 29.9 BMI = Overweight
+// 30 or higher BMI = Obese
+
+//#########################QUESTION 14###########################
+// write a checkSeason function. It takes month as the parameter and returns the corresponding season.
+
+//#########################QUESTION 15###########################
+// Write a function findMax. It takes three arguments and finds the max value. Don't use Math.max.
+
+//********************************LEVEL 2*******************
+//#########################QUESTION 1###########################
+// ax + by + c = 0. Using this formula write a solveLinEquationfunction.
+
+//#########################QUESTION 2###########################
+// ax^2 + bx + c = 0. Using this formula write a solveQuadEquation function.
+
+//#########################QUESTION 3###########################
+// Declare a printArray function. It takes an array as it's parameter and prints out the values of the array.
+
+//#########################QUESTION 4###########################
+// write a showDateTime function using the Date object. 09/26/ 2025 04:50.
+
+//#########################QUESTION 5###########################
+// Swap x and y using a swapValues function.
+// (43,53) // x => 43, y => 53
+
+//#########################QUESTION 6###########################
+// Declare a reverseArray function. It takes an array as a parameter and returns the array reversed. Don't use the reverse method.
+
+//#########################QUESTION 7###########################
+// write a capitalizeArray function.
+
+//#########################QUESTION 8###########################
+// Declare a function addItem that takes an item as a parameter and returns it as an array.
+
+//#########################QUESTION 9###########################
+// Declare a removeItem function. It takes index as a parameter and removes the item at the index from the array.
+
+//#########################QUESTION 10###########################
+// Declare a sumOfNumbers function. It takes number as a parameter and adds all the numbers in that range.
+
+//#########################QUESTION 11###########################
+// Declare a sumOfOdds function. It takes number as a parameter and adds all the odd numbers in that range.
+
+//#########################QUESTION 12###########################
+// Declare a sumOfEvens function. It takes number as a parameter and adds all the even numbers in that range.
+
+//#########################QUESTION 13###########################
+// Declare an evenAndOdds function. It takes a positive Integer as a parameter and returns the number of evens and odds in that range.
+
+//#########################QUESTION 14###########################
+// Write a function that takes any number of arguments and returns the sum of the arguments.
+
+//#########################QUESTION 15###########################
+// Write a randomUserIpGenerator function.
+
+//#########################QUESTION 16###########################
+// Write a randomMacAddressGenerator function.
+
+//#########################QUESTION 17###########################
+// Write a randomHexNumberGenerator function.
+
+//#########################QUESTION 18###########################
+// Write a userIdGenerator function. The id should be seven characters long.
+
+//******************************LEVEL 3*************************
+//#########################QUESTION 1###########################
+// write a userIdGeneratedByUser function. Using prompt() get the number of characters and the number of id's to be generated.
+
+//#########################QUESTION 2###########################
+// Write an rgbColorGenerator function.
+
+//#########################QUESTION 3###########################
+// Write an arrayOfHexaColors function that return an array of hexadecimal colors.
+
+//#########################QUESTION 4###########################
+// Write an arrOfRgbColors function which returns an array of rgb colors.
+
+//#########################QUESTION 5###########################
+// Write a convertHexaToRgb function.
+
+//#########################QUESTION 6###########################
+// Write a convertRgbToHexa function.
+
+//#########################QUESTION 7###########################
+// Write a generateColors functions which generates any number of Hexadecimal or rgb colors.
+
+//#########################QUESTION 8###########################
+// Write a shuffleArray function. That takes an array and shuffles it.
+
+//#########################QUESTION 9###########################
+// Write a factorial function. It takes a whole number as a parameter and returns it's factorial.
+
+//#########################QUESTION 10###########################
+// Write an isEmpty function that checks if it's parameter is empty or not.
+
+//#########################QUESTION 11###########################
+// Write a sum function that returns the sum of it's parameters.
+
+//#########################QUESTION 12###########################
+// Write a sumOfArrayItems function that checks if all the contents of array are number types. If yes return the sum, else return "Not all items are numbers."
+
+//#########################QUESTION 13###########################
+// Write a average function that takes an array and finds the average of the items. Make sure to perform some type checking.
+
+//#########################QUESTION 14###########################
+// Write a modifyArray function that takes an array as a parameter, removes the fifth item and returns the new array. If the length of the array is less than five it returns "Item not found".
+
+//#########################QUESTION 15###########################
+// Write an isPrime function which checks if a number is prime.
+
+//#########################QUESTION 16###########################
+// Write a function that checks if all the items in an array are unique.
+
+//#########################QUESTION 17###########################
+// Write a function that checks if all the items in an array are of the same data type.
+
+//#########################QUESTION 18###########################
+// Write an isValidVariable that checks if a variable is valid or not.
+
+//#########################QUESTION 19###########################
+// Write a function which returns an array of seven unique random numbers between 0 and 9.
+
+//#########################QUESTION 20###########################
+// Write a reverseCountries function that copies the countries array and reverses the original.
