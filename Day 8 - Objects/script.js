43a075e5420fa8f6407e019b85f678b68c92cd94
milesroark <babatunjia@gmail.com>
2025-10-18 04:08:11 -0400

    started day 8

---
+++ Day 8 - Objects/script.js
@@ -0,0 +1,50 @@
+//*************************LEVEL 1******************************
+//#########################QUESTION 1###########################
+// Create a dog object
+//        Log the object to the console.
+
+//#########################QUESTION 2###########################
+// Give the object a name,legs ,color ,age property and a bark method that returns woof woof.
+//#########################QUESTION 3###########################
+// Log the values of the dog object.
+//#########################QUESTION 4###########################
+// Give the dog object a breed property and a getDogInfo method.
+
+//********************************LEVEL 2*******************
+//#########################QUESTION 1###########################
+// Find the most skilled user in the users object.
+//#########################QUESTION 2###########################
+// Log all online users
+//#########################QUESTION 3###########################
+// Log how many users have more that 50 points.
+//#########################QUESTION 4###########################
+// Log all the MERN stack developers.
+//#########################QUESTION 5###########################
+// Add your name as a property to the users object without modifying the original object.
+//#########################QUESTION 6###########################
+// Log all the keys of the users object.
+//#########################QUESTION 7###########################
+// Log all the values of the users object.
+//#########################QUESTION 8###########################
+// Using the countries object, print the following info.
+//                 Country name
+//                 Capital
+//                 Populations
+//                 Languages
+
+//******************************LEVEL 3*************************
+//#########################QUESTION 1###########################
+// Create a personalAccount object:
+//      Give it a firstName, lastName, incomes and expenses properties.
+//      Give it a totalIncome, totalExpense, accountInfo, addIncome,    addExpense and accountBalance methods.
+// Keys and values for the methods are income: description. expenses: description.
+//#########################QUESTION 2###########################
+// Using the users2arr:
+//        Create a signUp function which allows users to add to the collection and/or inform them that the user already exists.
+//        Create a signIn function for logging into the application.
+
+//#########################QUESTION 3###########################
+// Using the products arr:
+//       Create a rateProduct function which rates the product.
+//       Create a averageRating function which calculates the average rating of a product.
+//       Create a likeProduct function. which allows a user to like or unlike a product.
